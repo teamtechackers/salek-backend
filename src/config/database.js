3a83b77cf1e42dfd4be094cd2be1c3bcc3b0bb6b
ghulamshahbazali <shahbazamanat93@gmail.com>
2025-10-17 14:37:20 +0500

    password change

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ const dbConfig = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: "AVNS_TCux6DEnM6zgNPeDLSJ",
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
