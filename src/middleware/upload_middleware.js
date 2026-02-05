96377a5fab6322570e7c021bf5ff3bf23c80a176
ghulamshahbazali <shahbazamanat93@gmail.com>
2026-02-05 10:47:35 +0500

    fix: increase image upload size limit to 20MB

--- src/middleware/upload_middleware.js
+++ src/middleware/upload_middleware.js
@@ -47,7 +47,7 @@ export const uploadVaccineImage = multer({
 export const uploadProfileImage = multer({
   storage: profileStorage,
   limits: {
-    fileSize: 5 * 1024 * 1024 // 5MB limit
+    fileSize: 20 * 1024 * 1024 // 20MB limit
   },
   fileFilter: fileFilter
 }).single('image');
